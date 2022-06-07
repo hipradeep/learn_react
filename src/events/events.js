65a22656a9b26e6d84cb8e9888c9e94a9409e4de
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-07 23:14:33 +0530

    update

--- src/events/events.js
+++ src/events/events.js
@@ -6,15 +6,18 @@ const evnt = function () {
     const shoot = () => {
         alert("Great Shossst!");
     }
+    function Ancher() {
+       return <a href="https://www.google.com">google</a>;
+    }
 
     return (
-
         <div className="content">
             <button onClick={shoot}>Take the shot!</button>
             <button onClick={shoot}>Take the shot!</button>
             <button onClick={shoot}>Take the shot!</button>
             <button onClick={shoot}>Take the shot!</button>
-            <button onClick={shoot}>Take the shot!</button>
+            <button onClick={shoot}>Take the shot!</button>  
+          <Ancher/>
         </div>
 
     );
--- src/exmaplecmpts/carcomponents.js
+++ src/exmaplecmpts/carcomponents.js
@@ -5,5 +5,4 @@ function CarRed(props) {
     return <h2>I am a { props.color.color }!</h2>;
   }
 
-
 export default CarRed;
