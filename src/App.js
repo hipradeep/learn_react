3b77fb8ff562d0bc22cc9286e16d3218ef1629a0
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-07 22:46:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+
+// import Compnt from "./nav1/navbar1";
+import Compnt from "./events/events";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return ( <Compnt />  );
+
 }
 
 export default App;
---
+++ src/events/events.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./events.css"
+
+const evnt = function () {
+
+    const shoot = () => {
+        alert("Great Shossst!");
+    }
+
+
+    return (
+
+        <div className="content">
+            <button onClick={shoot}>Take the shot!</button>
+            <button onClick={shoot}>Take the shot!</button>
+            <button onClick={shoot}>Take the shot!</button>
+            <button onClick={shoot}>Take the shot!</button>
+            <button onClick={shoot}>Take the shot!</button>
+        </div>
+
+    );
+
+}
+
+export default evnt;
